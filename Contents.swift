b33aba266cd0640233c74818ad103433c563d853
muhammadferdiansyah <dandyferdiansyah2801@gmail.com>
2021-03-16 13:37:40 +0700

    Hari pertama - Variabel, Integer, Multiline String

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
 var str = "Hello, playground"
+str = "dandy"
+
+// Integer
+var age = 21
+age = 22
+
+var uang_jajan = 1_000_000
+
+// multiline string
+var pantun = """
+   jalan-jalan ke kota medan
+   jangan lupa beli oleh-oleh
+   kalau anda orang beriman
+   jangan lupa beramal soleh
+"""
+
+
